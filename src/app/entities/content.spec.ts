9b25d6cc4f35fe7084651399d7d4f8474a1581de
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-02 23:22:33 -0300

    test: create notification content tests

---
+++ src/app/entities/content.spec.ts
@@ -0,0 +1,16 @@
+import { Content } from './content';
+
+describe('Notification content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('Do you want follow Maby?');
+    expect(content).toBeTruthy();
+  });
+
+  it('should not be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('Yes')).toThrow();
+  });
+
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('M'.repeat(241))).toThrow();
+  });
+});
