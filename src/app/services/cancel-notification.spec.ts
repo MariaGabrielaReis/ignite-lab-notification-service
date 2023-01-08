8534aa4e596e3e4e3eb50d744a9df64cfd01c5f2
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-08 10:38:35 -0300

    test: create test to cancel notification

---
+++ src/app/services/cancel-notification.spec.ts
@@ -0,0 +1,37 @@
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      content: new Content('some content'),
+      category: 'social',
+      recipientId: 'some-recipient',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({ notificationId: notification.id });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'some-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
