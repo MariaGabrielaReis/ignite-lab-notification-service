8635c9d1d71679462f19941009a835f91c2896f8
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-08 11:22:25 -0300

    feat: create use case to unread notification

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -52,6 +52,10 @@ export class Notification {
     this.props.readAt = new Date();
   }
 
+  public unread() {
+    this.props.readAt = null;
+  }
+
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
   }
